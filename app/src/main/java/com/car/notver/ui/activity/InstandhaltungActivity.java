dffef2496ac6d2f34651cf40af9c495adeaa4a4d
aq610428 <641897605@qq.com>
2020-06-22 16:13:32 +0800

    添加客户信息

--- app/src/main/java/com/car/notver/ui/activity/InstandhaltungActivity.java
+++ app/src/main/java/com/car/notver/ui/activity/InstandhaltungActivity.java
@@ -1,6 +1,8 @@
 package com.car.notver.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import com.car.notver.R;
 import com.car.notver.base.BaseActivity;
 
@@ -10,6 +12,7 @@ import com.car.notver.base.BaseActivity;
  * @name:设备维修
  */
 public class InstandhaltungActivity extends BaseActivity {
+    private TextView title_text_tv, title_left_btn;
 
     @Override
     protected void initCreate(Bundle savedInstanceState) {
@@ -18,11 +21,24 @@ public class InstandhaltungActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        title_text_tv = getView(R.id.title_text_tv);
+        title_left_btn = getView(R.id.title_left_btn);
+        title_left_btn.setOnClickListener(this);
+        title_text_tv.setText("设备维修");
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.title_left_btn:
+                finish();
+                break;
+        }
     }
 }
