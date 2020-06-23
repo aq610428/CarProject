79da56c495445b6a92a0697313d8497be1b4dcfe
aq610428 <641897605@qq.com>
2020-06-23 10:29:23 +0800

    添加客户信息

--- app/src/main/java/com/car/notver/util/SystemTools.java
+++ app/src/main/java/com/car/notver/util/SystemTools.java
@@ -43,7 +43,7 @@ public final class SystemTools {
 
     /******车牌号码检测*****/
     public static boolean isCarnumberNO(String carnumber) {
-        String carnumRegex = "[\u4e00-\u9fa5]{1}[A-Z]{1}[A-Z_0-9]{5}";
+        String carnumRegex = "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-Z]{1}(([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9 挂学警港澳]{1})";
         if (TextUtils.isEmpty(carnumber)) return false;
         else return carnumber.matches(carnumRegex);
     }
