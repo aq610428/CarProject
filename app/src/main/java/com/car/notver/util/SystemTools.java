edc46d2c5c7ed10eb73c6c70a01f5bb151779073
aq610428 <641897605@qq.com>
2020-06-23 10:46:48 +0800

    添加客户信息

--- app/src/main/java/com/car/notver/util/SystemTools.java
+++ app/src/main/java/com/car/notver/util/SystemTools.java
@@ -32,6 +32,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 
 /**
@@ -41,11 +42,13 @@ import java.util.List;
  */
 public final class SystemTools {
 
-    /******车牌号码检测*****/
-    public static boolean isCarnumberNO(String carnumber) {
-        String carnumRegex = "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-Z]{1}(([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9 挂学警港澳]{1})";
-        if (TextUtils.isEmpty(carnumber)) return false;
-        else return carnumber.matches(carnumRegex);
+    /**
+     *
+     * @return
+     */
+    public static boolean isCarnumberNO(String carNumber) {
+        String pattern = "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼]{1}(([A-HJ-Z]{1}[A-HJ-NP-Z0-9]{5})|([A-HJ-Z]{1}(([DF]{1}[A-HJ-NP-Z0-9]{1}[0-9]{4})|([0-9]{5}[DF]{1})))|([A-HJ-Z]{1}[A-D0-9]{1}[0-9]{3}警)|([A-HJ-Z]{1}[A-Z0-9]{1}[0-9]{3}港)|([A-HJ-Z]{1}[A-Z0-9]{1}[0-9]{3}澳)))|([0-9]{6}使)|((([沪粤川云桂鄂陕蒙藏黑辽渝]{1}A)|鲁B|闽D|蒙E|蒙H)[0-9]{4}领)|(WJ[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼·•]{1}[0-9]{4}[TDSHBXJ0-9]{1})|([VKHBSLJNGCE]{1}[A-DJ-PR-TVY]{1}[0-9]{5})";
+        return Pattern.matches(pattern, carNumber) == true ? true : false;
     }
 
     public static List<CustomerInfo> getListCustomerInfo() {
